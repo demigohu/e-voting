86c6e829fd4481de3a47a9981f69a0c947cdffd9
kopsus <emonsetio01@gmail.com>
2024-12-04 14:11:14 +0700

    responsive navbar

--- src/app/(private)/navbar/page.tsx
+++ src/app/(private)/navbar/page.tsx
@@ -25,23 +25,33 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 py-4 z-50 bg-gradient-to-b from-neutral-950 via-neutral-950 left-0 w-full flex items-center justify-center gap-20">
-      {itemHeader.map((item, index) => (
-        <Link
-          key={index}
-          href={item.link}
-          className={`${
-            isActiveLink(item.link) ? "text-white" : "text-neutral-400"
-          }`}
-        >
-          {item.label}
-        </Link>
-      ))}
+    <>
+      <nav className="fixed top-0 py-4 z-50 bg-gradient-to-b from-neutral-950 via-neutral-950 left-0 w-full flex items-center justify-around lg:justify-center lg:gap-20">
+        {itemHeader.map((item, index) => (
+          <Link
+            key={index}
+            href={item.link}
+            className={`${
+              isActiveLink(item.link) ? "text-white" : "text-neutral-400"
+            }`}
+          >
+            {item.label}
+          </Link>
+        ))}
 
-      <Button variant={"secondary"}>
-        <w3m-button />
-      </Button>
-      <ClaimToken />
-    </nav>
+        <Button variant={"secondary"} className="hidden md:block">
+          <w3m-button />
+        </Button>
+        <div className="hidden md:block">
+          <ClaimToken />
+        </div>
+      </nav>
+      <div className="flex items-center gap-4 justify-around z-50 fixed bottom-0 w-full lg:hidden px-5 py-3 bg-neutral-800">
+        <Button variant={"secondary"}>
+          <w3m-button />
+        </Button>
+        <ClaimToken />
+      </div>
+    </>
   );
 }
