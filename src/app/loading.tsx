c4dddebc3e1bf981c6d082c8be8e8cec3de25407
kopsus <emonsetio01@gmail.com>
2024-12-11 00:51:45 +0700

    create loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center text-center">
+      <p>loading...</p>
+    </div>
+  );
+};
+
+export default loading;
